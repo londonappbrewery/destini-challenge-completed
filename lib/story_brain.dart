63e9c9d3dce3832573599e4dc84a4d5322f02a9e
Angela <8798027+angelabauer@users.noreply.github.com>
2019-03-18 16:56:16 +0000

    Step 10 Complete

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -42,9 +42,6 @@ class StoryBrain {
   }
 
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-  String getChoice1() {
-    return _storyData[0].choice1;
-  }
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
 
