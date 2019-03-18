a28e44a736bd61cbb0f28d3f09068468005ba46e
Angela <8798027+angelabauer@users.noreply.github.com>
2019-03-18 16:58:04 +0000

    Step 13 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _StoryPageState extends State<StoryPage> {
                   color: Colors.red,
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
