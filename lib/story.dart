a683fc896001ad18eb1a921ad00c28b266805ab0
Angela <8798027+angelabauer@users.noreply.github.com>
2019-03-18 16:38:37 +0000

    Step 7 Completed

--- lib/story.dart
+++ lib/story.dart
@@ -6,5 +6,5 @@ class Story {
   String choice2;
 
   //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-  Story(this.storyTitle, this.choice1, this.choice2);
+  Story({this.storyTitle, this.choice1, this.choice2});
 }
